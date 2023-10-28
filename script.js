b4dc17458e486c6efac55624a5d1052184b58a20
akshayk122 <myselfakshay22@gmail.com>
2023-10-28 22:14:09 +0530

    second commit

--- script.js
+++ script.js
@@ -2,6 +2,5 @@ $('.portfolio-button').mousedown(function(){
     timeout = setInterval(function(){
         window.scrollBy(0,-1); // May need to be -1 to go down
     }, 0); // Play around with this number. May go too fast
-
     return false;
 });
